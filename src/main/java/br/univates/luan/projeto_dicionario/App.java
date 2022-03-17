4d7621deb8a4c2c67a1eaa41d6ea6ce2b008e527
Areial do Porto <43920813+luanhenzo@users.noreply.github.com>
2022-03-17 18:32:38 +0100

    Dicionrio est agora funcionando. Inserido 14 palavras no dicionrio.

--- src/main/java/br/univates/luan/projeto_dicionario/App.java
+++ src/main/java/br/univates/luan/projeto_dicionario/App.java
@@ -6,16 +6,19 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 
 import br.univates.luan.projeto_dicionario.entities.Dicionario;
+import br.univates.luan.projeto_dicionario.entities.Palavra;
 import br.univates.luan.projeto_dicionario.entities.Usuario;
 import br.univates.luan.projeto_dicionario.utilities.JSONUtils;
 
 public class App {
 
-    private static boolean validaExistenciaUsuario(Usuario usuarioTentandoEntrar, JSONArray listaDeUsuarios) {
+    private static final JSONArray LISTA_DE_USUARIOS = JSONUtils.readExternalJson("C:\\Users\\areia\\Documents\\Coisas do Luan\\Desenvolvimento\\Estudos\\projeto-dicionario\\src\\main\\java\\br\\univates\\luan\\projeto_dicionario\\entities\\usuarios.json");
+    
+    private static boolean validaExistenciaUsuario(Usuario usuarioTentandoEntrar) {
         boolean usuarioEncontrado = false;
-        if (usuarioTentandoEntrar != null && listaDeUsuarios != null) {
-            for(int i = 0; i < listaDeUsuarios.size(); i++) {
-                JSONObject usuarioNoJson = (JSONObject) listaDeUsuarios.get(i);
+        if (usuarioTentandoEntrar != null && LISTA_DE_USUARIOS != null) {
+            for(int i = 0; i < LISTA_DE_USUARIOS.size(); i++) {
+                JSONObject usuarioNoJson = (JSONObject) LISTA_DE_USUARIOS.get(i);
                 if (usuarioTentandoEntrar.getNomeDeUsuario().equals((String) usuarioNoJson.get("nome")) &&
                     usuarioTentandoEntrar.getSenha().equals((String) usuarioNoJson.get("senha"))) {
                         usuarioEncontrado = true;
@@ -26,15 +29,27 @@ public class App {
         return usuarioEncontrado;
     }
     
+    private static boolean iniciaApp() {
+        boolean appDesligado = false;
+        
+        Dicionario dicionarioLuan = new Dicionario("O Grande Dicionário do Luan", "C:\\Users\\areia\\Documents\\Coisas do Luan\\Desenvolvimento\\Estudos\\projeto-dicionario\\src\\main\\java\\br\\univates\\luan\\projeto_dicionario\\entities\\dicionario.json");
+        
+        for (Palavra palavra : dicionarioLuan.getPalavras()) {
+            System.out.println(palavra.getPalavra() + ": " + palavra.getSignificado());
+            System.out.println("Fonte: " + palavra.getFonte() + "\n");
+        }
+        
+        return appDesligado;
+    }
+    
     public static void main(String[] args) {
-        JSONArray jsonDeUsuarios = JSONUtils.readExternalJson("C:\\Users\\areia\\Documents\\Coisas do Luan\\Desenvolvimento\\Estudos\\projeto-dicionario\\src\\main\\java\\br\\univates\\luan\\projeto_dicionario\\entities\\usuarios.json");
         Scanner scanner = new Scanner(System.in);
         
         String nomeDeUsuario;
         String senhaDeAcesso;
         Usuario usuario = null;
         
-        while(!validaExistenciaUsuario(usuario, jsonDeUsuarios)) {
+        while(!validaExistenciaUsuario(usuario)) {
             System.out.print("Insiria o nome de usuário: ");
             nomeDeUsuario = scanner.nextLine();
             System.out.print("Insira a senha: ");
@@ -42,8 +57,9 @@ public class App {
 
             usuario = new Usuario(nomeDeUsuario, senhaDeAcesso);
 
-            if (validaExistenciaUsuario(usuario, jsonDeUsuarios)) {
+            if (validaExistenciaUsuario(usuario)) {
                 System.out.println("Logado como: " + usuario.getNomeDeUsuario() + "\n");
+                iniciaApp();
             } else {
                 System.out.println("Usuário ou senha inválido!\n");
             }
--- src/main/java/br/univates/luan/projeto_dicionario/entities/Dicionario.java
+++ src/main/java/br/univates/luan/projeto_dicionario/entities/Dicionario.java
@@ -1,23 +1,40 @@
 package br.univates.luan.projeto_dicionario.entities;
 
-import java.util.List;
+import java.util.ArrayList;
+import br.univates.luan.projeto_dicionario.utilities.JSONUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Dicionario {
     
     private final String nome;
-    private List<Palavra> palavras;
-    private String enderecoDicionario;
+    private final ArrayList<Palavra> palavras = new ArrayList<>();
+    private final String enderecoDicionario;
 
     public Dicionario(String nome, String enderecoDicionario) {
         this.nome = nome;
         this.enderecoDicionario = enderecoDicionario;
+        
+        JSONArray palavrasJsonArray = JSONUtils.readExternalJson(this.enderecoDicionario);
+        
+        for (Object palavraJson : palavrasJsonArray) {
+            JSONObject palavra = (JSONObject) palavraJson;
+            String palavraNome = (String) palavra.get("palavra");
+            String palavraSignificado = (String) palavra.get("significado");
+            String palavraFonte = (String) palavra.get("fonte");
+            this.palavras.add(new Palavra(palavraNome, palavraSignificado, palavraFonte));
+        }
     }
 
     public String getNome() {
         return nome;
     }
-
-    public List<Palavra> getAllPalavras() {
+    
+    public String getEnderecoDicionario() {
+        return this.enderecoDicionario;
+    }
+    
+    public ArrayList<Palavra> getPalavras() {
         return palavras;
     }
     
